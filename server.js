77c6c117c424b4875e0ec53de68b982b1644000d
ratanakchhay <ratanakchhay03@gmail.com>
2025-09-13 10:43:45 +0700

    added client folder

--- server.js
+++ server.js
@@ -17,6 +17,6 @@ app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/posts', require('./routes/api/posts'))
 app.use('/api/profile', require('./routes/api/profile'))
 
-const PORT = process.env.PORT || 3000
+const PORT = 5000
 
 app.listen(PORT, () => {console.log(`Server started on PORT ${PORT}.`)})
