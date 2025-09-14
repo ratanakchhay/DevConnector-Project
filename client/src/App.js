9a1c96e1b8a37e85d45b90b30bfd1b39de35ebc0
ratanakchhay <ratanakchhay03@gmail.com>
2025-09-14 19:21:15 +0700

    added routes in client

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Landing from './components/layout/Landing'
 import Navbar from './components/layout/Navbar'
+import Login from './components/auth/Login'
+import Register from './components/auth/Register'
 
 function App() {
   return (
     <>
+      <BrowserRouter>
       <Navbar />
-      <Landing />
+        <section>
+          <Routes>
+            <Route path = "/" element = {<Landing />} />
+            <Route path = "/login" element = {<Login />} />
+            <Route path = "/register" element = {<Register />} />
+          </Routes>
+        </section>
+      </BrowserRouter>
     </>
   )
 }
