26122de481c8c42ca7d12f6b518c8a7726c02044
ratanakchhay <ratanakchhay03@gmail.com>
2025-09-19 10:46:29 +0700

    added ui for logins

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,10 +1,46 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Login () {
+  const [ formData, setFormData ] = React.useState({
+    email: "",
+    password: "",
+  })
+
+  const handleSubmit = async (form) => {
+    const email = form.get('email')
+    const password = form.get('password') 
+    setFormData({ email, password })
+
+    console.log('Success')
+  }
+
   return (
-    <>
-      Login
-    </>
+    <section className="container">
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead"><i className="fas fa-user"></i> Sign into Your Account</p>
+      <form className="form" action={handleSubmit}>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Login" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Sign Up</Link>
+      </p>
+    </section>
   )
 }
 
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,10 +1,68 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Register () {
+  const [ formData, setFormData ] = React.useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: ""
+  })
+
+  const handleSubmit = async (form) => {
+    const name = form.get('name')
+    const email = form.get('email')
+    const password = form.get('password') 
+    const password2 = form.get('password2')
+
+    setFormData({ name, email, password, password2 })
+
+    if (password !== password2) {
+      console.error('Passwords do not match')
+    } else {
+      console.log('Success') 
+    }
+  }
+
   return (
-    <div>
-      Register
-    </div>
+    <section className = 'container'>
+      <h1 className="large text-primary">Sign Up</h1>
+      <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
+
+      <form className="form" action={handleSubmit}>
+        <div className="form-group">
+          <input type="text" placeholder="Name" name="name" required />
+        </div>
+        <div className="form-group">
+          <input type="email" placeholder="Email Address" name="email" />
+          <small className="form-text"
+            >This site uses Gravatar so if you want a profile image, use a
+            Gravatar email</small
+          >
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            minLength="6"
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="password2"
+            minLength="6"
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Register" />
+      </form>
+
+      <p className="my-1">
+        Already have an account? <Link to="/login">Sign In</Link>
+      </p>
+    </section>
   )
 }
 
